5ca48fbbff1d5f13d93ffd4c29356fdb66435377
Tristan Hudreaux <contact@brico-technique.fr>
2026-04-14 16:59:18 +0200

    initial commit Django sécurisé

--- machines/urls.py
+++ machines/urls.py
@@ -14,16 +14,12 @@ from .views import (
 
 urlpatterns = [
     path("recherche/", views.recherche_reglages, name="recherche_reglages"),
-
     path("reglage/new/", views.create_reglage, name="create_reglage"),
     path("reglage/<int:reglage_id>/", views.detail_reglage, name="detail_reglage"),
     path("reglage/<int:reglage_id>/edit/", views.edit_reglage, name="edit_reglage"),
     path("reglage/<int:reglage_id>/delete/", views.delete_reglage, name="delete_reglage"),
     path("reglage/<int:reglage_id>/cancel/", views.cancel_new_reglage, name="cancel_new_reglage"),
-
     path("api/of/", views.api_of_list, name="api_of_list"),
-
-    # ✅ API VRAC (celle pour remonter les infos instantanément)
     path("api/vrac/<int:vrac_id>/", views.api_vrac_detail, name="api_vrac_detail"),
     path("api/reglage/history/<str:ref>/", views.api_history_by_ref, name="api_history_by_ref"),
     path("api/vrac/usage/<int:vrac_id>/", views.api_vrac_usage, name="api_vrac_usage"),
